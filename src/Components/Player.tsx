805efc39da9f5e53130fbb255bbcb7ce9c153bdc
ashwin586 <ashwinv586@gmail.com>
2024-02-26 09:28:57 +0530

    player component bug fixed

--- src/Components/Player.tsx
+++ src/Components/Player.tsx
@@ -50,19 +50,25 @@ const Player: React.FC<PlayerProps> = ({
   const skipSongHandler = (action: string) => {
     const currentIndex = tracks.findIndex((track) => track.id === song.id);
     if (action === "skip-backward") {
-      setCurrentSong(tracks[currentIndex + (1 % tracks.length)]);
-      activeSongHandler(tracks[currentIndex + (1 % tracks.length)]);
-    }
-
-    if (action === "skip-forward") {
       if (currentIndex - (1 % tracks.length) === -1) {
         setCurrentSong(tracks[tracks.length - 1]);
         activeSongHandler(tracks[tracks.length - 1]);
         if (isPlaying) audioRef.current?.play();
         return;
       }
-      setCurrentSong(tracks[currentIndex - (1 % tracks.length)]);
-      activeSongHandler(tracks[currentIndex - (1 % tracks.length)]);
+      setCurrentSong(tracks[currentIndex - 1]);
+      activeSongHandler(tracks[currentIndex - 1]);
+    }
+
+    if (action === "skip-forward") {
+      if (currentIndex + (1 % tracks.length) >= tracks.length) {
+        setCurrentSong(tracks[tracks.length - 1 - currentIndex]);
+        activeSongHandler(tracks[tracks.length - 1 - currentIndex]);
+        if (isPlaying) audioRef.current?.play();
+        return;
+      }
+      setCurrentSong(tracks[currentIndex + (1 % tracks.length)]);
+      activeSongHandler(tracks[currentIndex + (1 % tracks.length)]);
     }
     if (isPlaying) audioRef.current?.play();
   };
@@ -73,40 +79,42 @@ const Player: React.FC<PlayerProps> = ({
       <div className="current-playing">
         <img src={song?.cover} alt="currentPlayingSongCover" />{" "}
         <span>
-          <h4>{song?.name}</h4>
-          <p>{song?.artist}</p>
+          <div className="song-details">
+            <h4>{song?.name}</h4>
+            <p>{song?.artist}</p>
+          </div>
         </span>
       </div>
-      <div className="track-controls">
-        <FontAwesomeIcon
-          className="skip-back"
-          size="2x"
-          icon={faAngleLeft}
-          onClick={() => skipSongHandler("skip-backward")}
-        />
-        <FontAwesomeIcon
-          className="play"
-          size="2x"
-          icon={isPlaying ? faPause : faPlay}
-          onClick={playSongHandler}
-        />
-        <FontAwesomeIcon
-          className="skip-forward"
-          size="2x"
-          icon={faAngleRight}
-          onClick={() => skipSongHandler("skip-forward")}
-        />
-      </div>
-      <div className="audio-controls">
-        <input
-          value={50}
-          min={0}
-          max={100}
-          type="range"
-          className="volume-range"
-          onChange={volumeHandler}
-        />
-      </div>
+        <div className="track-controls">
+          <FontAwesomeIcon
+            className="skip-back"
+            size="2x"
+            icon={faAngleLeft}
+            onClick={() => skipSongHandler("skip-backward")}
+          />
+          <FontAwesomeIcon
+            className="play"
+            size="2x"
+            icon={isPlaying ? faPause : faPlay}
+            onClick={playSongHandler}
+          />
+          <FontAwesomeIcon
+            className="skip-forward"
+            size="2x"
+            icon={faAngleRight}
+            onClick={() => skipSongHandler("skip-forward")}
+          />
+        </div>
+        <div className="audio-controls">
+          <input
+            value={50}
+            min={0}
+            max={100}
+            type="range"
+            className="volume-range"
+            onChange={volumeHandler}
+          />
+        </div>
     </>
   );
 };
